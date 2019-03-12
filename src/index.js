9f3dc8c6a4e49813db67280a015d6630ddb84d1b
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2019-03-12 21:07:36 +0100

    Usage of HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
+let root = document.createElement('div')
+document.body.appendChild(root)
+
 render(
-  <div>
-    <App/>
-  </div>,
-  document.getElementById("root")
+    <div>
+        <App/>
+    </div>,
+    root
 );
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,33 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 process.env.NODE_ENV = 'development'
 
 module.exports = {
-  devtool: 'eval',
-  mode: 'development',
-  entry: [
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      use: ['babel-loader'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
-};
+    devtool: 'eval',
+    mode: 'development',
+    entry: [
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Mobx + React'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            use: ['babel-loader'],
+            include: path.join(__dirname, 'src')
+        }]
+    }
+}
